f4f4a36200e0107603e5d25b9814a29b989c7998
francescobarbarulo <barbarulofrancesco@gmail.com>
2024-06-28 14:55:25 +0200

    fix bootstrap

--- lab/bootstrap.sh
+++ lab/bootstrap.sh
@@ -2,28 +2,30 @@
 
 # Install Docker
 curl https://get.docker.com | sh
-docker completion bash | sudo tee /etc/bash_completion.d/cilium
+docker completion bash | sudo tee /etc/bash_completion.d/docker > /dev/null
 # Add user to docker group
 usermod -aG docker $USER
 
 # Install kind
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-amd64
+KIND_VERSION=v0.23.0
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-linux-amd64
 chmod +x ./kind
 mv ./kind /usr/local/bin/kind
-kind completion bash | sudo tee /etc/bash_completion.d/kind
+kind completion bash | sudo tee /etc/bash_completion.d/kind > /dev/null
 
 # Install kubectl
 KUBECTL_VERSION=v1.29.4 # https://dl.k8s.io/release/stable.txt
 curl -LO https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
+chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
-kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
 
 # Install cilium CLI
 CILIUM_CLI_VERSION=v0.16.11 # https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt
 curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-amd64.tar.gz
 tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
 rm cilium-linux-amd64.tar.gz
-cilium completion bash | sudo tee /etc/bash_completion.d/cilium
+cilium completion bash | sudo tee /etc/bash_completion.d/cilium > /dev/null
 
 echo "======================================================================"
 echo
